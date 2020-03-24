64c8b74210f61bdd4120159c6826e9df6234976f
SPLoT <mahonen.samu@gmail.com>
2020-03-24 14:30:28 +0200

    Lopullinen muunnin

--- project.js
+++ project.js
@@ -1,31 +1,48 @@
 const celsiusInput = document.querySelector('#celsius > input')
 const fahrenheitInput = document.querySelector('#fahrenheit > input')
 const kelvinInput = document.querySelector('#kelvin > input')
+const Tyhjenna = document.querySelector('#tyhjenna > button')
+
+function ruondnum(num) {
+    return Math.round(num*100)/100;
+}
 
 function tempconvertCtoFandK() {
     const CelsiusTemp = Number(celsiusInput.value);
     const FahrenheitTemp = CelsiusTemp * (9/5) + 32;
     const KelvinTemp = CelsiusTemp + 273.15;
-    fahrenheitInput.value = FahrenheitTemp
-    kelvinInput.value = KelvinTemp
+    fahrenheitInput.value = ruondnum(FahrenheitTemp)
+    kelvinInput.value = ruondnum(KelvinTemp)
 }
 
 function tempconvertFtoCandK() {
     const FahrenheitTemp = Number(fahrenheitInput.value);
     const CelsiusTemp = (FahrenheitTemp - 32) * 5/9;
     const KelvinTemp = (FahrenheitTemp - 32) * 5/9 + 273.15;
-    celsiusInput.value = CelsiusTemp
-    kelvinInput.value = KelvinTemp
+    celsiusInput.value = ruondnum(CelsiusTemp)
+    kelvinInput.value = ruondnum(KelvinTemp)
 }
 
 function tempconvertKtoFandC() {
     const KelvinTemp = Number(kelvinInput.value);
     const FahrenheitTemp = 9/5 * (KelvinTemp - 273.15) + 32;
     const CelsiusTemp = KelvinTemp - 273.15;
-    fahrenheitInput.value = FahrenheitTemp
-    celsiusInput.value = CelsiusTemp
+    fahrenheitInput.value = ruondnum(FahrenheitTemp)
+    celsiusInput.value = ruondnum(CelsiusTemp)
 }
 
+function tyhja() {
+    celsiusInput.value = ''
+    fahrenheitInput.value = ''
+    kelvinInput.value = ''
+}
+
+function main() {
 celsiusInput.addEventListener('input', tempconvertCtoFandK);
 fahrenheitInput.addEventListener('input', tempconvertFtoCandK);
 kelvinInput.addEventListener('input', tempconvertKtoFandC);
+Tyhjenna.addEventListener('click', tyhja);
+}
+
+main();
+
